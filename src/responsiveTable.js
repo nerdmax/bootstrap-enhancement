42b6d2fd01a186289503cefae2d61d3807675c43
mrliucan <mrliucan@foxmail.com>
2018-05-10 10:24:15 +1000

    Fix some style issues

--- src/responsiveTable.js
+++ src/responsiveTable.js
@@ -1,56 +1,48 @@
-const $ = require("jquery");
+const $ = require('jquery');
+
+function displayResTableHeading(parentEle, displayMessage) {
+  const parentEleSafe =
+    parentEle === undefined ? document.getElementsByTagName('BODY')[0] : parentEle;
+  $(parentEleSafe)
+    .find('.table-responsive')
+    .each(() => {
+      $(this).each(() => {
+        // console.log($(this));
+        const tableWrapperWidth = $(this).width();
+        const tableWidth = $(this)
+          .find('table')
+          .width();
+        // console.log(tableWrapperWidth);
+        // console.log(tableWidth);
+
+        if (tableWrapperWidth !== 0 && tableWidth !== 0 && tableWidth > tableWrapperWidth) {
+          $(this).prepend(`<p class="table-responsive-heading">${
+            displayMessage === undefined ? 'Scroll horizontally for more details' : displayMessage
+          }</p>`);
+          // $(this)
+          //   .find(".table-responsive-heading")
+          //   .show();
+        }
+        // console.log(tableWrapperWidth);
+        // console.log(tableWidth);
+      });
+    });
+}
 
 // Init
-exports.init = function(displayMessage) {
-  setTimeout(function() {
+exports.init = function init(displayMessage) {
+  setTimeout(() => {
     displayResTableHeading(undefined, displayMessage);
   }, 1);
-  $(".collapse").on("show.bs.collapse", function() {
-    var that = this;
-    setTimeout(function() {
+  $('.collapse').on('show.bs.collapse', () => {
+    const that = this;
+    setTimeout(() => {
       displayResTableHeading(that, displayMessage);
     }, 1);
   });
 };
 
 // Force upate
-exports.update = function(displayMessage) {
+exports.update = (displayMessage) => {
   displayResTableHeading(undefined, displayMessage);
 };
-
-function displayResTableHeading(parentEle, displayMessage) {
-  if (parentEle == undefined) {
-    parentEle = document.getElementsByTagName("BODY")[0];
-  }
-  $(parentEle)
-    .find(".table-responsive")
-    .each(function() {
-      $(this).each(function() {
-        // console.log($(this));
-        var tableWrapperWidth = $(this).width();
-        var tableWidth = $(this)
-          .find("table")
-          .width();
-        // console.log(tableWrapperWidth);
-        // console.log(tableWidth);
-        if (
-          tableWrapperWidth != 0 &&
-          tableWidth != 0 &&
-          tableWidth > tableWrapperWidth
-        ) {
-          $(this).prepend(
-            `<p class="table-responsive-heading">${
-              displayMessage === undefined
-                ? "Scroll horizontally for more details"
-                : displayMessage
-            }</p>`
-          );
-          // $(this)
-          //   .find(".table-responsive-heading")
-          //   .show();
-        }
-        //console.log(tableWrapperWidth);
-        //console.log(tableWidth);
-      });
-    });
-}
