07fcd35119c45bc63be6fed30de8ce898d57f7c0
mrliucan <mrliucan@foxmail.com>
2018-05-17 12:36:38 +1000

    Add toggleCollapseChevron

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import * as bseUtilities from './bseUtilities/index';
 import * as addScrollMessToResTable from './addScrollMessToResTable/index';
 import * as fixModalShifting from './fixModalShifting/index';
+import * as toggleCollapseChevron from './toggleCollapseChevron/index';
 
-export { bseUtilities, addScrollMessToResTable, fixModalShifting };
+export { bseUtilities, addScrollMessToResTable, fixModalShifting, toggleCollapseChevron };
---
+++ src/toggleCollapseChevron/index.js
@@ -0,0 +1,30 @@
+import { getJquery } from '../bseUtilities/index';
+
+export function init() {
+  getJquery()
+    .then(($) => {
+      $('.collapse').on('show.bs.collapse', (event) => {
+        // console.log('show.bs.collapse');
+        // console.log(event);
+        // console.log(event.currentTarget);
+        const $chevronIcon = $(document)
+          .find(`[href='#${$(event.target).attr('id')}']`)
+          .find('svg');
+        // console.log($chevronIcon);
+        $chevronIcon.attr('data-icon', 'chevron-up');
+        // $(`#${this.id}-chevron`).toggleClass('fa-chevron-down fa-chevron-up');
+        // $(`#${this.id}-hvr`).toggleClass('hvr-icon-hang hvr-icon-bob');
+      });
+      $('.collapse').on('hide.bs.collapse', (event) => {
+        const $chevronIcon = $(document)
+          .find(`[href='#${$(event.target).attr('id')}']`)
+          .find('svg');
+        $chevronIcon.attr('data-icon', 'chevron-down');
+      });
+    })
+    .catch((e) => {
+      console.error(e);
+    });
+}
+
+export function update() {}
