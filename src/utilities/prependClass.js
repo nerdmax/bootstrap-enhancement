d61bf36427e4febecd55f70563b1fa4971c15945
mrliucan <mrliucan@foxmail.com>
2018-05-31 09:42:35 +1000

    Add annotation to prependClass

--- src/utilities/prependClass.js
+++ src/utilities/prependClass.js
@@ -1,5 +1,12 @@
 import getJquery from './../utilities/getJquery';
 
+/**
+ * Add class names to the left most position to avoid overwriting original class's style
+ * @example
+ * prependClass(domElement, 'test-class');
+ * @param {Object} sel - Dom element that you want to add class to.
+ * @param {string} strClass - Class name that you want add.
+ */
 export default function prependClass(sel, strClass) {
   getJquery().then(($) => {
     const $el = $(sel);
