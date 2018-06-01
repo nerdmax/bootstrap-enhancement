44371455d242a0315011eb1772bd3393232b1d76
mrliucan <mrliucan@foxmail.com>
2018-06-01 15:23:21 +1000

    Add some samples for testing

--- samples/index.js
+++ samples/index.js
@@ -1,15 +1,107 @@
-// const bse = require("../src/index");
-
-// console.log(bse.responsiveTable);
-
-// import * as bse from '../src/index';
-import $ from 'jquery';
-window.$ = $;
 import bse from '../dist/bootstrap-enhancement';
 
-// const bseReq = require('../dist/bootstrap-enhancement');
+const { $ } = window;
 
+console.log($);
 console.log(bse);
+
+$(() => {
+  if (document.title === 'Bootstrap Enhancement Samples - utilities - prependClass') {
+    console.log(bse.utilities.prependClass);
+    const testEle = document.getElementById('testDom');
+    bse.utilities.prependClass(testEle, 'text-success');
+  }
+
+  if (document.title === 'Bootstrap Enhancement Samples - utilities - getJquery') {
+    console.log(bse.utilities.getJquery);
+    bse.utilities.getJquery().then((jQueryFromWindow) => {
+      console.log(jQueryFromWindow);
+    });
+  }
+
+  if (document.title === 'Bootstrap Enhancement Samples - utilities - hasScrollBar') {
+    console.log(bse.utilities.hasScrollBar);
+    $('#checkScrollBar').on('click', () => {
+      alert(bse.utilities.hasScrollBar());
+    });
+  }
+
+  if (document.title === 'Bootstrap Enhancement Samples - utilities - getScrollBarWidth') {
+    console.log(bse.utilities.getScrollBarWidth);
+    $('#getScrollBar').on('click', () => {
+      alert(bse.utilities.getScrollBarWidth());
+    });
+  }
+
+  if (document.title === 'Bootstrap Enhancement Samples - utilities - addStyles') {
+    console.log(bse.utilities.addStyles);
+    $('#addStyles').on('click', () => {
+      bse.utilities.addStyles(`
+        .test{color: red}
+      `);
+    });
+  }
+
+  if (document.title === 'Bootstrap Enhancement Samples - utilities - checkBreakPoint') {
+    console.log(bse.utilities.checkBreakPoint);
+    $('#checkSize').on('click', () => {
+      console.log(bse.utilities.checkBreakPoint.checkSize());
+    });
+    $('#is').on('click', () => {
+      console.log(bse.utilities.checkBreakPoint.is('>md'));
+    });
+  }
+
+  if (document.title === 'Bootstrap Enhancement Samples - utilities - addToWindow') {
+    console.log(bse.utilities.addToWindow);
+    $('#addToWindow').on('click', () => {
+      bse.utilities.addToWindow('test', { name: 'testObject' });
+      console.log(window.test);
+    });
+  }
+
+  if (document.title === 'Bootstrap Enhancement Samples - addScrollMessToResTable') {
+    console.log(bse.addScrollMessToResTable);
+    $('#addScrollMessToResTableInit').on('click', () => {
+      bse.addScrollMessToResTable.init();
+    });
+    $('#addScrollMessToResTableUpdate').on('click', () => {
+      bse.addScrollMessToResTable.update();
+    });
+  }
+
+  if (document.title === 'Bootstrap Enhancement Samples - fixModalShifting') {
+    console.log(bse.fixModalShifting);
+    $('#fixModalShiftingInit').on('click', () => {
+      bse.fixModalShifting.init();
+    });
+  }
+
+  // TODO: Need to load font awesome to make it working
+  if (document.title === 'Bootstrap Enhancement Samples - toggleCollapseChevron') {
+    console.log(bse.toggleCollapseChevron);
+    // $('#fixModalShiftingInit').on('click', () => {
+    //   bse.fixModalShifting.init();
+    // });
+  }
+
+  if (document.title === 'Bootstrap Enhancement Samples - scrollToTop') {
+    console.log(bse.scrollToTop);
+    // $('#scrollToTopInit').on('click', () => {
+    //   bse.scrollToTop.init('TESTSETTSE');
+    // });
+    $(() => {
+      bse.scrollToTop.init('TESTSETTSE');
+    });
+  }
+
+  if (document.title === 'Bootstrap Enhancement Samples - resList') {
+    console.log(bse.resList);
+    $('#resListInit').on('click', () => {
+      bse.resList.init();
+    });
+  }
+});
 
 bse.resList.init();
 
--- samples/test.js
+++
@@ -1,3 +0,0 @@
-// const test = require('../dist/bse');
-
-// console.log(test);
